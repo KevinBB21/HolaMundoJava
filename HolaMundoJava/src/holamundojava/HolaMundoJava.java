3393665bf6ee7ddc3aeac9d7168763039337f248
a023326273h <a023326273h@S09-PC15>
2021-11-22 19:24:30 +0100

    Programa Hola Mundo

---
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto HolaMundoJava - Archivo HolaMundoJava.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package holamundojava;
+
+/**
+ *
+ * @author Kevin Blanco Bañuls <blancodaw22@gmail.com>
+ * @version 1.0
+ * @date 22 nov. 2021 19:08:07
+ */
+public class HolaMundoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
