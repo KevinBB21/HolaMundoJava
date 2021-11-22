fb779a7adcec37ef6874c8156f0fa3ff31639f9d
KevinBB22 <blancodaw22@gmail.com>
2021-11-22 19:26:33 +0100

    Añadido el print.

--- HolaMundoJava/src/holamundojava/HolaMundoJava.java
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -18,7 +18,9 @@ public class HolaMundoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        System.out.println("Hola Mundo");
+        
     }
 
 }
